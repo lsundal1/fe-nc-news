686c525ef268f75bc621c00cc0e3df59ec7f5b11
lsundal1 <126904972+lsundal1@users.noreply.github.com>
2024-11-11 12:28:25 +0000

    updated article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -8,8 +8,8 @@ export default function ArticleCard ({item}) {
         <div className="article-card">
             <img className="article-card" id="article-card-img" src={item.article_img_url} alt="article-card-img"></img> 
             <Link to={url}><h3>{item.title}</h3></Link>
-            <ul>
-                <li>Author: {item.author}   |   Date Published: {item.created_at.split('T')[0]}</li>            
+            <ul className="article-card" id="info">
+                <li>Author: {item.author}   |   {item.created_at.split('T')[0]}</li>            
                 <li>Topic: {item.topic}</li>
                 <li>Votes: {item.votes}</li>
             </ul>
